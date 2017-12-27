8e98b79153559067deefe8c7de9477bfa23beaa3
Elad Alfassa <ealfassa@redhat.com>
2017-12-27 12:02:17 +0200

    Move environment variables creation to a script
    You can run this script to generate an environment file,
    so it's easier to use a control host which is not the
    cluster master.

---
+++ make_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo export OPENSHIFT_CFME_ROUTE=\""$(oc get route --namespace='openshift-management' -o go-template --template='{{.spec.host}}' httpd 2> /dev/null)"\"
+echo export OPENSHIFT_HAWKULAR_ROUTE=\""$(oc get route --namespace='openshift-infra' -o go-template --template='{{.spec.host}}' hawkular-metrics 2> /dev/null)"\"
+echo export OPENSHIFT_PROMETHEUS_ALERTS_ROUTE=\""$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' alerts 2> /dev/null)"\"
+echo export OPENSHIFT_PROMETHEUS_METRICS_ROUTE=\""$(oc get route --namespace='openshift-metrics' -o go-template --template='{{.spec.host}}' prometheus 2> /dev/null)"\"
+echo export OPENSHIFT_MASTER_HOST=\""$(oc get nodes -o name |grep master |sed -e 's/nodes\///g')"\"
+echo export OPENSHIFT_MANAGEMENT_ADMIN_TOKEN=\""$(oc sa get-token -n management-infra management-admin)"\"
+echo export OPENSHIFT_CA_CRT=\""$(cat /etc/origin/master/ca.crt)"\"
+echo export OPENSHIFT_PROVIDER_NAME=\""OCP37"\"
+echo export OPENSHIFT_CFME_USER=\""admin"\"
+echo export OPENSHIFT_CFME_PASS=\""smartvm"\"
+echo export OPENSHIFT_CFME_AUTH=\"'${OPENSHIFT_CFME_USER}:${OPENSHIFT_CFME_PASS}'\"
